1cf71c61038c48ca38f20c85975d555a2a7056da
Murilo <murkuehne@gmail.com>
2025-06-24 22:23:59 -0300

    Finalizando as rotas de autorização e autenticação

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -2,11 +2,18 @@ import { Router } from "express"
 import { ProductsController } from "@/controllers/products-controller"
 
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+// productsRoutes.use(verifyUserAuthorization(["admin", "user"]))
 productsRoutes.get("/", productsController.index)
-productsRoutes.post("/", ensureAuthenticated, productsController.create)
+productsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale", "admin", "costumer"]),
+  productsController.create
+)
 
 export { productsRoutes }
